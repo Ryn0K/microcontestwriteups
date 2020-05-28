4135851a3c6b133f87c8dedabbcbf17c068ddf79
karan bhargav <44202642+krn-bhargav@users.noreply.github.com>
2020-05-28 16:32:24 +0530

    Update sumoftwonumbers.py

--- sumoftwonumbers.py
+++ sumoftwonumbers.py
@@ -1,6 +1,9 @@
-import libmicrocontest,sys,os,subprocess
+#!/usr/bin/python
+#Written by krn_bhargav(kapslock)
+from libmicrocontest2_python27 import * # External library given by website should be in same directory.
+import sys,os,subprocess
 
-c = libmicrocontest.commence_contest(1,'kapslock','')
+c = commence_contest(1,'kapslock','')
 a = c.get_int('a')
 b = c.get_int('b')
 s = a+b
