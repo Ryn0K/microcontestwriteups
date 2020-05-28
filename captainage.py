782465b3b6f2110bf330fbd05085658f4558a456
karan bhargav <44202642+krn-bhargav@users.noreply.github.com>
2020-05-28 16:23:29 +0530

    Update captainage.py

--- captainage.py
+++ captainage.py
@@ -1,5 +1,8 @@
-import libmicrocontest,sys,os,subprocess
-c = libmicrocontest.commence_contest(14,'kapslock','')
+#!/usr/bin/python
+#Written by krn_bhargav(kapslock)
+from libmicrocontest2_python27 import * # External library given by website should be in same directory.
+import sys,os,subprocess
+c = commence_contest(14,'kapslock','')
 birth = c.get_int('naissance')
 current = c.get_int('date_courante')
 age = current - birth
