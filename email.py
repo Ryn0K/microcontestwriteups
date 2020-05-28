eef0758d318e17a9f115c495bb003c2361feb01a
karan bhargav <44202642+krn-bhargav@users.noreply.github.com>
2020-05-28 16:27:38 +0530

    Update email.py

--- email.py
+++ email.py
@@ -1,10 +1,13 @@
-import libmicrocontest,sys,time,os,subprocess,imaplib,email
+#!/usr/bin/python
+#Written by krn_bhargav(kapslock)
+from libmicrocontest2_python27 import * # External library given by website should be in same directory.
+import sys,time,os,subprocess,imaplib,email
 
 def main():
-    username = "kkbhargavmail@gmail.com"
-    password = "110727272"
+    username = ""
+    password = ""
     imap_server  = "imap.gmail.com"
-    c = libmicrocontest.commence_contest(21,'kapslock','')
+    c = commence_contest(21,'kapslock','')
     number = c.get_int('number')
     print("[+]Number : "+str(number))
 
